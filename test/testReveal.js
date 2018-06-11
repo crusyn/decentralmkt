2889e2781cb2747e9273e428ee15c1efd32798e1
Chris Rusyniak <c.rusyn@gmail.com>
2018-06-11 17:11:46 -0400

    formatting

--- test/testReveal.js
+++ test/testReveal.js
@@ -4,7 +4,7 @@ EcommerceStore.deployed().then(function(i) {i.addProductToStore('iphone 6', 'Cel
 EcommerceStore.deployed().then(function(i) {i.getProduct.call(1).then(function(f) {console.log(f)})})
 Eutil = require('ethereumjs-util');
 sealedBid = '0x' + Eutil.sha3((2 * amt_1) + 'mysecretacc1').toString('hex');
-  EcommerceStore.deployed().then(function(i) {i.bid(1, sealedBid, {value: 3*amt_1, from: web3.eth.accounts[1]}).then(function(f) {console.log(f)})});
+EcommerceStore.deployed().then(function(i) {i.bid(1, sealedBid, {value: 3*amt_1, from: web3.eth.accounts[1]}).then(function(f) {console.log(f)})});
 sealedBid = '0x' + Eutil.sha3((3 * amt_1) + 'mysecretacc2').toString('hex');
 EcommerceStore.deployed().then(function(i) {i.bid(1, sealedBid, {value: 4*amt_1, from: web3.eth.accounts[2]}).then(function(f) {console.log(f)})});
 
@@ -15,3 +15,4 @@ EcommerceStore.deployed().then(function(i) {i.revealBid(1, (2*amt_1).toString(),
 EcommerceStore.deployed().then(function(i) {i.revealBid(1, (3*amt_1).toString(), 'mysecretacc2', {from: web3.eth.accounts[2]}).then(function(f) {console.log(f)})})
 
 EcommerceStore.deployed().then(function(i) {i.highestBidderInfo.call(1).then(function(f) {console.log(f)})})
+EcommerceStore.deployed().then(function(i) {i.totalBids.call(1).then(function(f) {console.log(f)})})
